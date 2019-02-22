1418c3a6e7e17844c2afb4a6c5f9821b3af23148
Joao Costa <joaocosta@zonalivre.org>
2019-02-22 09:51:33 +0000

    support pulling and pushing

--- bin/gitPullAll.sh
+++ bin/gitPullAll.sh
@@ -2,7 +2,14 @@
 
 set -euo pipefail
 
+NAME=$(basename $0)
+if [ "$NAME" == "gitPullAll.sh" ]; then
+  mode=pull
+else
+  mode=push
+fi
+
 for f in $PWD/*;
 do
-    [ -d "$f"/.git ] && echo "Pulling $f" && cd "$f" && git pull
+    [ -d "$f"/.git ] && echo "$mode $f" && cd "$f" && git $mode
 done
